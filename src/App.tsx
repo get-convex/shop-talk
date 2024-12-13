08944f83cc170e9a0740f0be0d19b3cd986c2ed6
Mike Cann <mike.cann@gmail.com>
2024-12-13 14:51:28 +0800

    changed over to openai

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
-import { RTVIClient } from "@pipecat-ai/client-js";
+import {
+  FunctionCallParams,
+  LLMHelper,
+  RTVIClient,
+} from "@pipecat-ai/client-js";
 import { useState } from "react";
 import { DailyTransport } from "@pipecat-ai/daily-transport";
 import { RTVIClientAudio, RTVIClientProvider } from "@pipecat-ai/client-react";
@@ -22,7 +26,7 @@ function App() {
           services: {
             stt: "deepgram",
             tts: "cartesia",
-            llm: "anthropic",
+            llm: "openai",
           },
         },
         endpoints: {
@@ -44,7 +48,7 @@ function App() {
             options: [
               {
                 name: "model",
-                value: "claude-3-5-sonnet-latest",
+                value: "gpt-4o-mini",
               },
               {
                 name: "initial_messages",
@@ -54,7 +58,7 @@ function App() {
                     content: [
                       {
                         type: "text",
-                        text: "You are a pirate.",
+                        text: "You are a hippy.",
                       },
                     ],
                   },
@@ -64,10 +68,64 @@ function App() {
                 name: "run_on_config",
                 value: true,
               },
+              {
+                name: "tools",
+                value: [
+                  {
+                    type: "function",
+                    function: {
+                      name: "get_current_weather",
+                      description:
+                        "Get the current weather for a location. This includes the conditions as well as the temperature.",
+                      parameters: {
+                        type: "object",
+                        properties: {
+                          location: {
+                            type: "string",
+                            description:
+                              "The city and state, e.g. San Francisco, CA",
+                          },
+                          format: {
+                            type: "string",
+                            enum: ["celsius", "fahrenheit"],
+                            description:
+                              "The temperature unit to use. Infer this from the users location.",
+                          },
+                        },
+                        required: ["location", "format"],
+                      },
+                    },
+                  },
+                ],
+              },
             ],
           },
         ],
       },
+    });
+
+    // Below the RTVIClient instance you created
+    const llmHelper = newVoiceClient.registerHelper(
+      "llm",
+      new LLMHelper({
+        callbacks: {},
+      })
+    ) as LLMHelper;
+
+    llmHelper.handleFunctionCall(async (fn: FunctionCallParams) => {
+      const args = fn.arguments as any;
+
+      console.log("----- FUNCTION CALL", fn);
+
+      if (fn.functionName === "get_current_weather" && args.location) {
+        const response = await fetch(
+          `/api/weather?location=${encodeURIComponent(args.location)}`
+        );
+        const json = await response.json();
+        return json;
+      } else {
+        return { error: "couldn't fetch weather" };
+      }
     });
 
     setVoiceClient(newVoiceClient);
